bcb4bd82ccd56e84dfdeb5e480d5ff48631beb8a
Erica S. Francisco <erykatdb@yahoo.com.br>
2022-10-11 23:10:34 -0300

    feat: 🎸 first steps - class

---
+++ criacao-de-classe.js
@@ -0,0 +1,25 @@
+class Pessoa {
+    constructor(nome, sobrenome){
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+
+    falar(){
+        console.log(`${this.nome} está falando`);
+    }
+
+    comer(){
+        console.log(`${this.nome} está comendo`);
+    }
+
+    beber(){
+        console.log(`${this.nome} está bebendo`);
+    }
+}
+
+const p1 = new Pessoa('Luiz','Miranda');
+const p2 = new Pessoa('Maria','Miranda');
+const p3 = new Pessoa('Joana','Miranda');
+const p4 = new Pessoa('Larissa','Miranda');
+
+console.log(p1.falar());
